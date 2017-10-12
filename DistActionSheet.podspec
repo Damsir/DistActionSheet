8756bc9b23521795c6cdd3c86de671bcc90f05a3
Damrin <75081647@qq.com>
2017-10-12 14:44:37 +0800

    1

---
+++ DistActionSheet.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+
+  s.name         = "DistActionSheet"
+  s.version      = "1.0.0"
+  s.summary      = "Like WeChat ActionSheet"
+  s.author       = { "Damrin" => "75081647@qq.com" }
+  s.homepage    = 'https://github.com/Damsir/DistActionSheet'
+  s.source      = { :git => 'https://github.com/Damsir/DistActionSheet.git', :tag => s.version }
+  s.license = "MIT"
+  s.platform = :ios, "8.0"
+  s.requires_arc = true
+  s.source_files = "DistActionSheet", "DistActionSheet/**/*.{h,m}"
+  # s.public_header_files = "DistSlideSegment/*.h"
+  s.framework = 'UIKit'
+  s.ios.deployment_target = "8.0"
+
+end
---
+++ DistActionSheet/DistActionSheet.h
@@ -0,0 +1,78 @@
+/*
+ 作者：  吴定如 <wudr@dist.com.cn>
+ 文件：  DistActionSheet.h
+ 版本：  1.0.0
+ 地址：  https://github.com/Damsir/DistActionSheet
+ 描述：  类似微信ActionSheet控件,支持横竖屏切换,视图直接放置于keyWindow上
+ */
+
+#import <UIKit/UIKit.h>
+
+@class DistActionSheet;
+
+/**
+ * block回调
+ *
+ * @param actionSheet LPActionSheet对象自身
+ * @param index       被点击按钮标识,取消: 0, 删除: -1, 其他: 1.2.3...
+ */
+typedef void(^DistActionSheetBlock)(DistActionSheet *actionSheet, NSInteger index);
+
+@interface DistActionSheet : UIView
+
+/**
+ * 创建DistActionSheet对象
+ *
+ * @param title                  提示文本
+ * @param cancelButtonTitle      取消按钮文本
+ * @param destructiveButtonTitle 删除按钮文本
+ * @param otherButtonTitles      其他按钮文本
+ * @param actionSheetBlock       block回调
+ *
+ * @return LPActionSheet对象
+ */
+- (instancetype)initWithTitle:(NSString *)title
+            cancelButtonTitle:(NSString *)cancelButtonTitle
+       destructiveButtonTitle:(NSString *)destructiveButtonTitle
+            otherButtonTitles:(NSArray *)otherButtonTitles
+                      handler:(DistActionSheetBlock)actionSheetBlock NS_DESIGNATED_INITIALIZER;
+
+/**
+ * 创建LPActionSheet对象(便利构造器)
+ *
+ * @param title                  提示文本
+ * @param cancelButtonTitle      取消按钮文本
+ * @param destructiveButtonTitle 删除按钮文本
+ * @param otherButtonTitles      其他按钮文本
+ * @param actionSheetBlock       block回调
+ *
+ * @return LPActionSheet对象
+ */
++ (instancetype)actionSheetWithTitle:(NSString *)title
+                   cancelButtonTitle:(NSString *)cancelButtonTitle
+              destructiveButtonTitle:(NSString *)destructiveButtonTitle
+                   otherButtonTitles:(NSArray *)otherButtonTitles
+                             handler:(DistActionSheetBlock)actionSheetBlock;
+
+/**
+ * 弹出LPActionSheet视图
+ *
+ * @param title                  提示文本
+ * @param cancelButtonTitle      取消按钮文本
+ * @param destructiveButtonTitle 删除按钮文本
+ * @param otherButtonTitles      其他按钮文本
+ * @param actionSheetBlock       block回调
+ *
+ */
++ (void)showActionSheetWithTitle:(NSString *)title
+               cancelButtonTitle:(NSString *)cancelButtonTitle
+          destructiveButtonTitle:(NSString *)destructiveButtonTitle
+               otherButtonTitles:(NSArray *)otherButtonTitles
+                         handler:(DistActionSheetBlock)actionSheetBlock;
+
+/**
+ * 弹出视图
+ */
+- (void)show;
+
+@end
